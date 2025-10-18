69a2f735eba671002a7604d47ec634d84d2fe023
seo-inst <innerpeace.deepi@gmail.com>
2025-10-18 11:07:11 +0900

    feat: ec2 domain name으로  연동

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -5,8 +5,8 @@ import axios from "axios";
 // axios 인스턴스 생성 - 공통 설정을 담은 axios 복사본을 만들어 'api'라는 이름으로 사용
 const api = axios.create({
   // baseURL: "http://localhost:8080", // 로컬 Spring API 서버 주소
-  baseURL: "http://43.200.3.84:8080", // my aws ec2 서버주소
-  //baseURL: "https://joy9.store", // AWS EC2 Spring API 서버 주소
+  //baseURL: "http://43.200.3.84:8080", // my aws ec2 서버주소
+  baseURL: "https://trust9.store", //https로 요청, 기본포트 443, ALB <-> AWS EC2 Spring API 서버 주소
   headers: {
     "Content-Type": "application/json",
   },
